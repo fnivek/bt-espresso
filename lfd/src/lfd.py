68314237bb3e38ec88c20c0af820858529060dc0
Kevin French <fnivek@gmail.com>
2018-06-15 16:52:36 -0400

    CENTROIDS: Detect centroids with py_trees

--- lfd/src/lfd.py
+++ lfd/src/lfd.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python
 import select
 import sys
+import itertools
 
 from sklearn.svm import LinearSVC
 import numpy
 
 import rospy
 import actionlib
+from std_msgs.msg import String
 
 import py_trees
 
 from centroid_detector_msgs.msg import DetectCentroidGoal, DetectCentroidAction
-from behavior_manager.interfaces.manipulation_behavior import FullyExtendTorso, ColapseTorso, MoveTorsoBehavior
+from behavior_manager.interfaces.manipulation_behavior import FullyExtendTorso, ColapseTorso, MoveTorsoBehavior, PickBehavior
+from behavior_manager.interfaces.centroid_detector_behavior import CentroidDetectorBehavior
 
 class SimpleTree:
     def __init__(self, behav):
@@ -29,26 +32,28 @@ class LfD:
         # Initilise the node
         rospy.init_node('lfd')
 
-        # Centroid detector
-        self.goal = DetectCentroidGoal()
-        self.goal.min_x = 0
-        self.goal.max_x = 1.5
-        self.goal.min_y = -0.5
-        self.goal.max_y = 0.5
-        self.goal.min_z = 0.6
-        self.goal.max_z = 2
-        self.client = actionlib.SimpleActionClient('/centroid_detector', DetectCentroidAction)
-        rospy.loginfo('Waiting for centroid detector action server')
-        self.client.wait_for_server()
-        rospy.loginfo('Centroid detector action server is running')
-
         # Demonstrations
         self.states = None
         self.actions = None
 
         # Actions
-        self.hi = SimpleTree(FullyExtendTorso('hi'))
-        self.bye = SimpleTree(MoveTorsoBehavior('Bye', 0.2))
+        self.extend_torso = SimpleTree(FullyExtendTorso('extend_torso'))
+        self.mid_torso = SimpleTree(MoveTorsoBehavior('mid_torso', 0.2))
+        self.pick = SimpleTree(PickBehavior('pick'))
+        self.detect_centroid = SimpleTree(CentroidDetectorBehavior('detect_centroid'))
+
+        # Blackboard setup
+        self.blackboard = py_trees.blackboard.Blackboard()
+        self.blackboard.set('detect_centroid/min_x', 0)
+        self.blackboard.set('detect_centroid/max_x', 1.5)
+        self.blackboard.set('detect_centroid/min_y', -0.5)
+        self.blackboard.set('detect_centroid/max_y', 0.5)
+        self.blackboard.set('detect_centroid/min_z', 0.6)
+        self.blackboard.set('detect_centroid/max_z', 2)
+        self.blackboard.set('shelf', String('shelf2'))
+        print 'Detecting a centroid to write to init blackboard cause I am lazy'
+        self.detect_centroid.run_once()
+        print 'State:', self.blackboard
 
     # A model that takes in a state and produces an action
     def model(self, state):
@@ -60,36 +65,28 @@ class LfD:
         clf.fit(states, actions)
         return clf.predict
 
-    def detect_centroid(self):
-        # Send a goal to centroid detector
-        rospy.loginfo('Detecting centroid')
-        self.client.send_goal(self.goal)
-        self.client.wait_for_result()
-        result = self.client.get_result()
-        # Convert result to a numpy array
-        result = numpy.array([
-            [
-                result.centroid.position.x,
-                result.centroid.position.y,
-                result.centroid.position.z,
-                result.centroid.orientation.x,
-                result.centroid.orientation.y,
-                result.centroid.orientation.z,
-                result.centroid.orientation.w,
-                result.success
-            ]
-        ])
-        return result
-
     def execute(self):
-        state = self.detect_centroid()
-        print 'World state is:\n' + str(state)
+        self.detect_centroid.run_once()
+        state = self.get_state()
+        print 'World state is:\n' + str(self.blackboard)
         action = self.model(state)[0]
         print 'Resulting action is: ' + action
         if action == 'h':
-            self.hi.run_once()
+            self.extend_torso.run_once()
         else:
-            self.bye.run_once()
+            self.mid_torso.run_once()
+
+    def get_state(self):
+        return numpy.array([[
+            self.blackboard.centroid.centroid.position.x,
+            self.blackboard.centroid.centroid.position.y,
+            self.blackboard.centroid.centroid.position.z,
+            self.blackboard.centroid.centroid.orientation.x,
+            self.blackboard.centroid.centroid.orientation.y,
+            self.blackboard.centroid.centroid.orientation.z,
+            self.blackboard.centroid.centroid.orientation.w,
+            self.blackboard.centroid.success
+        ]])
 
     def run(self):
         # State
@@ -110,22 +107,28 @@ class LfD:
                         self.model = self.learn(self.states, self.actions)
                         state = 'AskUser'
                     elif user_input == 'e':
-                        self.execute()
-                        state = 'AskUser'
+                        state = 'Execute'
                     else:
                         state = 'AskUser'
             elif state == 'Demonstrate':
                 print 'Demonstrate'
-                result = self.detect_centroid()
-                print 'World state is\n' + str(result)
+                self.detect_centroid.run_once()
+                world_state = self.get_state()
+                print 'World state is\n' + str(self.blackboard)
                 user_input = numpy.array([[raw_input('What action should be taken; say hi (h) or nop (n):')]])
                 if self.states is not None:
-                    self.states = numpy.append(self.states, result, axis=0)
+                    self.states = numpy.append(self.states, world_state, axis=0)
                     self.actions = numpy.append(self.actions, user_input)
                 else:
-                    self.states = result
+                    self.states = world_state
                     self.actions = user_input
                 state = 'AskUser'
+            elif state == 'Execute':
+                print 'Press enter to end execution'
+                self.execute()
+                if select.select([sys.stdin,], [], [], 0.0)[0]:
+                    raw_input()
+                    state = "AskUser"
 
             # Sleep if needed
             sleep_rate.sleep()
