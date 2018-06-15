03abe1d6002226348cba1ad0ab4acd3dae9b2d8a
Kevin French <fnivek@gmail.com>
2018-06-15 14:42:28 -0400

    CLASS: Convert functions into class

--- lfd/src/lfd.py
+++ lfd/src/lfd.py
@@ -23,124 +23,117 @@ class SimpleTree:
         while self.behav.status == py_trees.Status.RUNNING:
             self.behav.tick_once()
 
-# Global vars
-# Construct a goal for the centroid detector
-goal = DetectCentroidGoal()
-goal.min_x = 0
-goal.max_x = 1.5
-goal.min_y = -0.5
-goal.max_y = 0.5
-goal.min_z = 0.6
-goal.max_z = 2
-client = actionlib.SimpleActionClient('/centroid_detector', DetectCentroidAction)
-states = None
-actions = None
-hi = None
-bye = None
+class LfD:
+    def __init__(self):
+        # Init ros
+        # Initilise the node
+        rospy.init_node('lfd')
 
-# A model that takes in a state and produces an action
-def model(state):
-    return 'h'
+        # Centroid detector
+        self.goal = DetectCentroidGoal()
+        self.goal.min_x = 0
+        self.goal.max_x = 1.5
+        self.goal.min_y = -0.5
+        self.goal.max_y = 0.5
+        self.goal.min_z = 0.6
+        self.goal.max_z = 2
+        self.client = actionlib.SimpleActionClient('/centroid_detector', DetectCentroidAction)
+        rospy.loginfo('Waiting for centroid detector action server')
+        self.client.wait_for_server()
+        rospy.loginfo('Centroid detector action server is running')
 
-# Produces a model from states
-def learn(states, actions):
-    clf = LinearSVC()
-    clf.fit(states, actions)
-    return clf.predict
+        # Demonstrations
+        self.states = None
+        self.actions = None
 
-def detect_centroid():
-    # Send a goal to centroid detector
-    rospy.loginfo('Detecting centroid')
-    client.send_goal(goal)
-    client.wait_for_result()
-    result = client.get_result()
-    # Convert result to a numpy array
-    result = numpy.array([
-        [
-            result.centroid.position.x,
-            result.centroid.position.y,
-            result.centroid.position.z,
-            result.centroid.orientation.x,
-            result.centroid.orientation.y,
-            result.centroid.orientation.z,
-            result.centroid.orientation.w,
-            result.success
-        ]
-    ])
-    return result
+        # Actions
+        self.hi = SimpleTree(FullyExtendTorso('hi'))
+        self.bye = SimpleTree(MoveTorsoBehavior('Bye', 0.2))
 
-def execute():
-    state = detect_centroid()
-    print 'World state is:\n' + str(state)
-    action = model(state)[0]
-    print 'Resulting action is: ' + action
-    act = None
-    if action == 'h':
-        hi.run_once()
-    else:
-        bye.run_once()
+    # A model that takes in a state and produces an action
+    def model(self, state):
+        return 'h'
+
+    # Produces a model from states
+    def learn(self, states, actions):
+        clf = LinearSVC()
+        clf.fit(states, actions)
+        return clf.predict
+
+    def detect_centroid(self):
+        # Send a goal to centroid detector
+        rospy.loginfo('Detecting centroid')
+        self.client.send_goal(self.goal)
+        self.client.wait_for_result()
+        result = self.client.get_result()
+        # Convert result to a numpy array
+        result = numpy.array([
+            [
+                result.centroid.position.x,
+                result.centroid.position.y,
+                result.centroid.position.z,
+                result.centroid.orientation.x,
+                result.centroid.orientation.y,
+                result.centroid.orientation.z,
+                result.centroid.orientation.w,
+                result.success
+            ]
+        ])
+        return result
+
+    def execute(self):
+        state = self.detect_centroid()
+        print 'World state is:\n' + str(state)
+        action = self.model(state)[0]
+        print 'Resulting action is: ' + action
+        if action == 'h':
+            self.hi.run_once()
+        else:
+            self.bye.run_once()
+
+    def run(self):
+        # State
+        state = 'AskUser'
+
+        # Loop every 10hz
+        sleep_rate = rospy.Rate(10)
+        while not rospy.is_shutdown():
+            if state == 'AskUser':
+                print 'Demonstrate(d), learn(l), execute(e):'
+                state = 'WaitForUser'
+            elif state == 'WaitForUser':
+                if select.select([sys.stdin,], [], [], 0.0)[0]:
+                    user_input = raw_input()
+                    if user_input == 'd':
+                        state = 'Demonstrate'
+                    elif user_input == 'l':
+                        self.model = self.learn(self.states, self.actions)
+                        state = 'AskUser'
+                    elif user_input == 'e':
+                        self.execute()
+                        state = 'AskUser'
+                    else:
+                        state = 'AskUser'
+            elif state == 'Demonstrate':
+                print 'Demonstrate'
+                result = self.detect_centroid()
+                print 'World state is\n' + str(result)
+                user_input = numpy.array([[raw_input('What action should be taken; say hi (h) or nop (n):')]])
+                if self.states is not None:
+                    self.states = numpy.append(self.states, result, axis=0)
+                    self.actions = numpy.append(self.actions, user_input)
+                else:
+                    self.states = result
+                    self.actions = user_input
+                state = 'AskUser'
+
+            # Sleep if needed
+            sleep_rate.sleep()
+
 
 def main():
-    global model
-    global states
-    global actions
-    global hi
-    global bye
-    # Initilise the node
-    rospy.init_node('lfd')
-
-    # Define an action client for centroid detector
-    rospy.loginfo('Waiting for centroid detector action server')
-    client.wait_for_server()
-    rospy.loginfo('Centroid detector action server is running')
-
-    # Define actions
-    hi = SimpleTree(FullyExtendTorso('hi'))
-    bye = SimpleTree(MoveTorsoBehavior('Bye', 0.2))
-
-    # State
-    state = 'AskUser'
-
-    # Loop every 10hz
-    sleep_rate = rospy.Rate(10)
-    while not rospy.is_shutdown():
-        if state == 'AskUser':
-            print 'Demonstrate(d), learn(l), execute(e):'
-            state = 'WaitForUser'
-        elif state == 'WaitForUser':
-            if select.select([sys.stdin,], [], [], 0.0)[0]:
-                user_input = raw_input()
-                if user_input == 'd':
-                    state = 'Demonstrate'
-                elif user_input == 'l':
-                    state = 'Learn'
-                elif user_input == 'e':
-                    state = 'Execute'
-                else:
-                    state = 'AskUser'
-        elif state == 'Execute':
-            print 'Execute'
-            execute()
-            state = 'AskUser'
-        elif state == 'Learn':
-            print 'Learning'
-            model = learn(states, actions)
-            state = 'AskUser'
-        elif state == 'Demonstrate':
-            print 'Demonstrate'
-            result = detect_centroid()
-            print 'World state is\n' + str(result)
-            user_input = numpy.array([[raw_input('What action should be taken; say hi (h) or nop (n):')]])
-            if states is not None:
-                states = numpy.append(states, result, axis=0)
-                actions = numpy.append(actions, user_input)
-            else:
-                states = result
-                actions = user_input
-            state = 'AskUser'
-
-        # Sleep if needed
-        sleep_rate.sleep()
+    lfd = LfD()
+    lfd.run()
 
 if __name__ == '__main__':
     main()
