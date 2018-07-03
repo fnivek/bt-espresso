47d6e154d3577d0bb6ea778c6b31d9ac59007752
Thomas Cohn <cohnt@umich.edu>
2018-07-03 15:05:19 -0400

    JOINTS: Add action to update joint states in blackboard. Ensure blackboard values are properly initialized during setup.

--- lfd/src/action_builders.py
+++ lfd/src/action_builders.py
@@ -2,11 +2,13 @@ import py_trees
 import py_trees_ros
 
 from std_msgs.msg import String
+from sensor_msgs.msg import JointState
 
 from centroid_detector_msgs.msg import DetectCentroidGoal, DetectCentroidAction
 from behavior_manager.interfaces.manipulation_behavior import FullyExtendTorso, ColapseTorso, MoveTorsoBehavior, PickBehavior, TuckWithCondBehavior, PlaceBehavior
 from behavior_manager.interfaces.centroid_detector_behavior import CentroidDetectorBehavior
 from behavior_manager.interfaces.head_actuate_behavior import HeadMoveBehavior
+from behavior_manager.interfaces.update_joints_behavior import JointToBlackboardBehavior
 
 class Action:
     def __init__(self, name, builder):
@@ -52,3 +54,5 @@ def BuildPlaceBehavior(name):
     return PlaceBehavior(name)
 def BuildHeadMoveBehavior(name):
     return HeadMoveBehavior(name, None, 1, 0, 0.5)
+def BuildUpdateJointsBehavior(name):
+    return JointToBlackboardBehavior(name=name, topic_name="/joint_states", topic_type=JointState)
