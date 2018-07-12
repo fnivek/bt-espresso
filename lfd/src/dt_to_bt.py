97e434709632104bba785ac72f3d13646af93140
Kevin French <fnivek@gmail.com>
2018-07-12 13:25:31 -0400

    DT_TO_BT: Start a standalone dt to bt conversion file

---
+++ lfd/src/dt_to_bt.py
@@ -0,0 +1,18 @@
+"""Decision tree to behavior tree.
+
+This file contains the code for converting decision trees to behavior trees.
+There are two methods that can be used.
+TODO(...): Explain methods.
+
+"""
+def dt_to_min_sop(true_children, false_children, clf):
+    """Convert a decision tree to a minimum sum of product representation.
+
+    inputs:
+        true_children: TODO(Allen).
+        fals_children: TODO(Allen).
+        clf: TODO(Allen).
+    outputs:
+        TODO(Allen).
+    """
+    pass
