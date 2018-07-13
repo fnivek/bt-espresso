96e71b1d376ae9463c2548bd8c8e3773360d9d5b
Kevin French <fnivek@gmail.com>
2018-07-13 13:32:59 -0400

    TEST: Test functions for dt_to_bt.py

--- lfd/tests/dt_to_bt_test.py
+++ lfd/tests/dt_to_bt_test.py
@@ -1,4 +1,41 @@
-from lfd import dt_to_bt
+from lfd.dt_to_bt import *
 
-def test():
-    print dt_to_bt
+true_children = [1, -1, 3, 4, -1, -1, -1]
+false_children = [2, -1, 6, 5, -1, -1, -1]
+clf = [-1, 1, -1, -1, 1, 2, 3]
+min_sop = {
+    1: [[(0, True)], [(2, True), (3, True)]],
+    2: [[(0, False), (2, True), (3, False)]],
+    3: [[(0, False), (2, False)]]
+}
+
+def dt_to_min_sop_test():
+    print 'Start dt_to_min_sop_test'
+    min_sop = dt_to_min_sop(true_children, false_children, clf)
+    print min_sop
+
+
+def min_sop_to_bt_cond_test():
+    print 'Start min_sop_to_bt_cond_test'
+    for sop in min_sop.values():
+        cond = min_sop_to_bt_cond(sop)
+        print sop
+        print cond
+
+
+def min_sops_to_bt_test():
+    print 'Start min_sops_to_bt_test'
+    bt = min_sops_to_bt(min_sop)
+    print bt
+
+
+def dt_to_bt_test():
+    print 'Start dt_to_bt_test'
+    bt = dt_to_bt(true_children, false_children, clf)
+    print bt
+
+
+def simple_dt_to_bt_test():
+    print 'Start simple_dt_to_bt_test'
+    bt = simple_dt_to_bt(true_children, false_children, clf)
+    print bt
